78871d21cbe9267979819afa1f20a6ff80e7ab77
MESBAHI-Nour <s3487637@gmail.com>
2026-09-25 13:42:14 +0000

    Delete LiveCoding directory session 1

--- LiveCoding/config/connexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-    try {
-        $connexion=new PDO("mysql:host=localhost;dbname=plateforme_streaming;port=3306","root","root");
-    } 
-    catch (Exeption $e) {
-        echo $e;
-    }
-?>    
-    
